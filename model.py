11f50609109ce32dfeab84333e8219ab8b708e45
aiden ward <aiden@MacBook-Pro-2.local>
2016-05-07 15:06:23 -0700

    skills done

---
+++ model.py
@@ -0,0 +1,96 @@
+"""Models and database functions for Ratings project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# Here's where we create the idea of our database. We're getting this through
+# the Flask-SQLAlchemy library. On db, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Part 1: Compose ORM
+
+class Model(db.Model):
+    """Car model."""
+
+    __tablename__ = "models"
+
+    name = db.Column(db.String(50), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    brand_name = db.Column(db.String(50), db.ForeignKey('brands.name'))
+
+    brand = db.relationship("Brand",
+                             backref=db.backref("models", order_by=name))
+
+    def __repr__(self):
+        """provide helpful representation when printed"""
+        
+        return "<Model id=%s name=%s year=%s brand_name=%s>" % (
+                                                                self.id,
+                                                                self.name,
+                                                                self.year,
+                                                                self.brand_name)
+
+class Brand(db.Model):
+    """Car brand."""
+
+    __tablename__ = "brands"
+    
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    founded = db.Column(db.Integer, nullable=True)
+    headquarters = db.Column(db.String(50), nullable=True)
+    discontinued = db.Column(db.Integer, nullable=True)
+
+    def __repr__(self):
+        """provides helpful representation when printed"""
+
+        return "<Brand id=%s name=%s founded=%s discontinued=%s>" % (
+                                                                self.id,
+                                                                self.name,
+                                                                self.founded,
+                                                                self.discontinued)
+
+
+    
+
+
+# End Part 1
+
+
+##############################################################################
+# Helper functions
+
+def init_app():
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///cars'
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
---
+++ query.py
@@ -0,0 +1,108 @@
+"""
+
+This file is the place to write solutions for the
+skills assignment called skills-sqlalchemy. Remember to
+consult the exercise directions for more complete
+explanations of the assignment.
+
+All classes from model.py are being imported for you
+here, so feel free to refer to classes without the
+[model.]User prefix.
+
+"""
+
+from model import *
+
+init_app()
+
+# -------------------------------------------------------------------
+# Part 2: Write queries
+
+
+# Get the brand with the **id** of 8.
+db.session.query(Brand).get(8)
+# Get all models with the **name** Corvette and the **brand_name** Chevrolet.
+Model.query.filter( (Model.name == 'Corvette') & (Model.brand_name == 'Chevrolet')).all()
+# Get all models that are older than 1960.
+Model.query.filter(Model.year < 1960).all()
+
+# Get all brands that were founded after 1920.
+Brand.query.filter(Brand.founded > 1920).all()
+# Get all models with names that begin with "Cor".
+Model.query.filter(Model.name.like( 'Cor%')).all()
+# Get all brands that were founded in 1903 and that are not yet discontinued.
+Brand.query.filter((Brand.founded == 1903)& (Brand.discontinued == None)).all()
+# Get all brands that are either 1) discontinued (at any time) or 2) founded 
+# before 1950.
+Brand.query.filter((Brand.founded < 1950)| (Brand.discontinued != None)).all()
+
+# Get any model whose brand_name is not Chevrolet.
+Model.query.filter(Model.brand_name !='Chevrolet').all()
+
+# Fill in the following functions. (See directions for more info.)
+
+def get_model_info(year):
+    '''Takes in a year, and prints out each model, brand_name, and brand
+    headquarters for that year using only ONE database query.'''
+    models = Model.query.options(db.joinedload(brand)).all()
+
+    for model in models:
+        if model.year == year:
+            if model.brand is not None:
+                
+                print "Brand name: %s, Model Name: %s, Headquarters: %s" % (
+                                                        model.brand_name,
+                                                        model.name,
+                                                        model.headquarters)
+            else:
+                print "Brand name: %s, Model Name: %s, --" % (
+                                                        model.brand_name,
+                                                        model.name)
+
+
+
+def get_brands_summary():
+    '''Prints out each brand name, and each model name for that brand
+     using only ONE database query.'''
+
+    models = Model.query.all()
+
+    for model in models:
+        print model.name, model.brand_name
+
+# -------------------------------------------------------------------
+# Part 2.5: Discussion Questions (Include your answers as comments.)
+
+# 1. What is the returned value and datatype of ``Brand.query.filter_by(name='Ford')``?
+# the data type is a flask-sqlaclhemy baseQuery type object
+# the value is, i dont know. if you exicute the query you get the brand object
+# that the query returns. but if you dont exicute the query then you are just
+# getting the memory location of the query object you created. I am not sure what
+# answer you are looking for as I am not sure I understand the question.
+
+# 2. In your own words, what is an association table, and what *type* of relationship
+# does an association table manage?
+# an association table manages a many to many relationship. Ie. in the example
+# given in the lecture. a book can have many genres and a genre can have many 
+# books. therefore you put an association table "between" the book and genre
+# tables to keep track of just book_ids and genre_ids that match. 
+# association tables just hold ids. 
+
+# -------------------------------------------------------------------
+# Part 3
+
+def search_brands_by_name(mystr):
+    #I dont get it. I have tried looking at the notes, flask-sqlalchemy docs,
+    # sqlalchemy docs and I have not been able to find out what i am doing wrong
+    # I have no idea how to get the .like() to work with a variable.
+    
+    brands = Brand.query.filter((Brand.name.like('%mystr%'))|(Brand.name == mystr)).all()
+    
+    return brands
+
+
+def get_models_between(start_year, end_year):
+    models = Model.query.filter((Model.year >= start_year) & (Model.year < end_year)).all()
+
+    return models
+    
